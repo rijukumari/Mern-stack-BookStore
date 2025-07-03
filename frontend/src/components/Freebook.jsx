71f65b453f8488f0965158de3ea501c450f414de
rijukumari <riju9314@gmail.com>
2025-07-03 15:05:18 +0530

    replaced backend url to deployed one.

--- frontend/src/components/Freebook.jsx
+++ frontend/src/components/Freebook.jsx
@@ -11,7 +11,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:3000/book");
+        const res = await axios.get(`${import.meta.env.VITE_BACKEND_LINK}/book`);
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
         setBook(data);
